e7bfc67a839a41f86db049f948d37bcf64552bba
BuildTools <unconfigured@null.spigotmc.org>
2019-06-20 01:23:31 +0800

    Completed Project

--- Assets/Scripts/BattleHandler.cs
+++ Assets/Scripts/BattleHandler.cs
@@ -15,22 +15,38 @@ public static class BattleHandler
 {
     public static void Battle(BattleEventData data)
     {
-        int botlvl = data.npc.level*3+1;
-        int playerlvl = data.player.level * 3 + 1;
+        /*  This is the battle logic for my game
+            Basically I times the level of the dancer by 3, then add one to ensure that the range matches the maximum number so that instead of between 1 - 3 
+            (which would be 2) it's 1 - 4 which means there is a chance to roll more than 1 number. Then I added on the Rhythm and Style stats as flat modifiers.
+            with luck, I wanted to times it by a random range between the luck stat and 1 adding on 1 to compensate for the range limitations.
+         */
+        // This is the bot's battle logic.
+        int botlvl = data.npc.level * 3 + 1 + data.npc.rhythm + data.npc.style + ((data.npc.luck) * Random.Range(1, (data.npc.luck+1)));
+        // This is the player's battle logic.
+        int playerlvl = data.player.level * 3 + 1 + data.player.rhythm + data.player.style + ((data.player.luck) * Random.Range(1, (data.player.luck+1)));
 
+        // So here I am calculating the actual dice roll to determine who wins.
         int bot = Random.Range(1, botlvl);
         int player = Random.Range(1, playerlvl); 
 
+        // I am initialising the outcome variable to use it later on.
         float outcome;
+        // So if the player's dice roll is higher than the bot's; the player wins.
         if (player > bot)
         {
             outcome = 1;
+
         }
+        // If not, the player loses.
         else
         {
-            outcome = 0;
+            outcome = -1;
         }
-         
+
+        // This is for debug purposes so that I can see the dice rolls.
+        Debug.Log("Player" + player);
+        Debug.Log("Bot" + bot);
+
         var results = new BattleResultEventData(data.player, data.npc, outcome);
 
         GameEvents.FinishedBattle(results);
--- Assets/Scripts/XPHandler.cs
+++ Assets/Scripts/XPHandler.cs
@@ -18,7 +18,7 @@ using UnityEngine;
 
 public class XPHandler : MonoBehaviour
 {
-    string lvlup = "Congratulations you have levelled up!";
+    
 
 
     private void OnEnable()
@@ -33,39 +33,48 @@ public class XPHandler : MonoBehaviour
 
     public void GainXP(BattleResultEventData data)
     {
+        //This executes if the player wins the battle.
         if (data.outcome == 1)
         {
+            //I want to give the player exp based on their level times 10.
             data.player.XP += data.player.level *10;
+            //When the NPC loses they still gain a small amount of exp based on their level times 4.
             data.npc.XP += data.npc.level * 4;
 
 
         }
+        //This executes if the player loses the battle
         else
         {
             data.player.XP += data.player.level * 4;
-            data.npc.XP += data.npc.level * 10;
+            //I want the NPC to gain exp at a slower rate to ensure that it's not too unfair.
+            data.npc.XP += data.npc.level * 8;
 
                
             }
-    
-                if (data.player.XP >= 200)
-            {
+        
+        //This levels the player up if their exp exceeds 100 times their present level plus 50, this then goes on to give the player additional stats for levelling up.
+        if (data.player.XP >= 100 * data.player.level+50)
+        {
                 data.player.XP = 0;
                 data.player.level += 1;
                 data.player.luck += 1;
                 data.player.style += 1;
                 data.player.rhythm += 1;
-                Debug.Log(lvlup);
+                //This is just a string that displays a level up message when the player levels up.
+                Debug.Log("Congratulations, you have reached level " + data.player.level);
 
-            }
-            if (data.npc.XP >= 200)
-            {
+        }
+
+        //This is the same as above but it applies to the NPC.
+        if (data.npc.XP >= 100 * data.npc.level + 50)
+        {
                 data.npc.XP = 0;
                 data.npc.level += 1;
                 data.npc.luck += 1;
                 data.npc.style += 1;
                 data.npc.rhythm += 1;
          
-            }
+        }
     }
 }
